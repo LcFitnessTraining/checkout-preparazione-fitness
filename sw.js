1bb86fd36f0dfa3e1c1610cc010b0eb4cae9ce59
LcFitnessTraining <lcfitnesstrainingpt@gmail.com>
2025-10-08 16:54:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,14 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   console.log('Service Worker attivato');
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      // Se la fetch fallisce (offline), puoi mostrare una risposta di fallback
+      return new Response('Offline', { status: 503, statusText: 'Offline' });
+    })
+  );
 });
